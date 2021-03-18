8ddce6b845ee5411295c72ccb464fb3850b6e243
Emrecabuk1987 <pentagonmc1@gmail.com>
2021-03-18 23:50:51 +0300

    18.gün 3 ders

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,18 +10,18 @@ import { ListResponseModel } from '../models/listResponseModel';
 })
 export class ProductService {
 
-  apiUrl = "https://localhost:44323/api/"
+  apiUrl = "https://localhost:44323/api/";
 
   constructor(private httpClient: HttpClient) { }
 
   getProducts(): Observable<ListResponseModel<Product>> {
-    let newPath=this.apiUrl + "producst/getall"
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl)
+    let newPath = this.apiUrl + "products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath)
   }
 
   getProductsByCategory(categoryId:number): Observable<ListResponseModel<Product>> {
-    let newPath=this.apiUrl + "producst/getbycategory?categoryId="+categoryId
-    return this.httpClient.get<ListResponseModel<Product>>(newPath)
+    let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 
 }
